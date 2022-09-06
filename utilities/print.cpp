1596fbbbbe007bafef191942abb3484f4004a1fa
Yunhan <89314250+yunhanluo@users.noreply.github.com>
2022-09-05 18:16:32 -0700

    Create print.cpp

---
+++ utilities/print.cpp
@@ -0,0 +1,13 @@
+void pv(vector<int> v) {
+  for (int i=0; i<v.size(); i++) {
+    cout << v[i] << " ";
+  }
+  cout << endl;
+}
+
+void pa(int a[], int length) {
+  for (int i=0; i<length; i++) {
+    cout << a[i] << " ";
+  }
+  cout << endl;
+}
